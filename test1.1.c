5dfa53690e931df7e4a9a6a53023929f1d709197
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-24 22:01:08 +0600

    Add files via upload

---
+++ test1.1.c
@@ -0,0 +1,10 @@
+//decimal to hexa-decimal
+#include<stdio.h>
+int main(void)
+{
+    int number;
+    printf("decimal number =");
+    scanf("%d",&number);
+    printf("hexa-decimal number =%x",number);
+    getch();
+}
